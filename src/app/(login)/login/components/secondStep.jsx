c3631aad50a7e38084eb1e52827b84db63811f98
DavgaaDavka08 <iderbatdagvadorj@gmail.com>
2025-11-24 11:31:31 +0800

    done

--- src/app/(login)/login/components/secondStep.jsx
+++ src/app/(login)/login/components/secondStep.jsx
@@ -1,6 +1,5 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-
 import { useRouter } from "next/navigation";
 //signin
 export default function SecondPage({
